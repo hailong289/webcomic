cda83ba5453056252ff626f9b84b6368d8cf8a3f
hailong289 <longdhps13563@fpt.edu.vn>
2022-01-01 15:44:41 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeNGConfig } from 'primeng/api';
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
