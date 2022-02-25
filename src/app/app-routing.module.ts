9fdd05fd2b56f79e50f4898f42e394b772489fba
hailong289 <longdh2.dev@gmail.com>
2022-02-25 17:28:06 +0700

    conflig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuardLoadService } from './auth/popmanage/auth-guard-load.service';
+import { AuthGuardLoadService as AuthGuardLoadAdmin  } from './auth/popmanage/auth-guard-load.service';
 import { HomeComponent } from './manage/home.component';
-import { AuthGuardService } from './auth/popmanage/auth-guard.service';
+import { AuthGuardService as AuthGuardAdmin } from './auth/popmanage/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -13,15 +13,25 @@ const routes: Routes = [
   {
     path:'admin/login',
     loadChildren: () => import("./manage/login/login.module").then( m => m.LoginModule),
-    canLoad: [AuthGuardLoadService],
+    canLoad: [AuthGuardLoadAdmin],
   },
   {
     path: 'admin',
     component: HomeComponent,
-    canActivate: [AuthGuardService],
+    canActivate: [AuthGuardAdmin],
     children: [
-        { path: 'dashboard', loadChildren: () => import('./manage/dasboard/dasboard.module').then(m => m.DasboardModule) },
-        { path: 'category',  loadChildren: () => import('./manage/category/category.module').then(m=> m.CategoryModule) },
+        { path: '', loadChildren: () => import('./manage/dasboard/dasboard.module').then(m => m.DasboardModule) },
+        { path: 'category',  loadChildren: () => import('./manage/category/category.module').then(m=> m.CategoryModule)},
+
+    ]
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuardAdmin],
+    children: [
+        { path: '', loadChildren: () => import('./manage/dasboard/dasboard.module').then(m => m.DasboardModule) },
+        { path: 'category',  loadChildren: () => import('./manage/category/category.module').then(m=> m.CategoryModule)},
 
     ]
   }
@@ -29,10 +39,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-
    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule],
-  providers: [AuthGuardLoadService]
+  providers: [AuthGuardAdmin]
 })
 export class AppRoutingModule { }
